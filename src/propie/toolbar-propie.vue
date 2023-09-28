ac7574c05091b4f63922c9b07af2b9e69500c419
DarkArcher822 <112427427+nicolaszagal@users.noreply.github.com>
2023-09-27 19:10:59 -0500

    Creacion de Archivos - Nicolás Zagal

--- src/propie/toolbar-propie.vue
+++ src/propie/toolbar-propie.vue
@@ -1,11 +1,80 @@
-<script setup>
-
-</script>
-
 <template>
-
+  <pv-toolbar class="custom-toolbar">
+    <template #start>
+      <img src="/public/favicon.ico" alt="Logo">
+    </template>
+    <template #end>
+      <div class="flex-column">
+        <router-link
+            v-for="item in items"
+            :to="item.to"
+            custom
+            v-slot="{ navigate, href }"
+            :key="item.label"
+        >
+          <pv-button
+              class="p-button-text text-black-alpha-90"
+              :href="href"
+              @click="navigate"
+              :class="{ 'active-button': $route.path === item.to }"
+          >{{ item.label }}</pv-button>
+        </router-link>
+        <router-link
+            to="/perfil"
+            custom
+            v-slot="{ navigate, href }"
+        >
+          <pv-button
+              class="p-button-text text-black-alpha-90 profile-button"
+              :href="href"
+              @click="navigate"
+          >
+            <i class="pi pi-user"></i>
+          </pv-button>
+        </router-link>
+      </div>
+    </template>
+  </pv-toolbar>
 </template>
 
+<script>
+export default{
+  props: {
+    items: Array,
+    currentPath:String,
+  }
+}
+</script>
+
 <style scoped>
+.custom-toolbar {
+  background-color: white;
+  padding: 10px;
+  border: none;
+  position: absolute;;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 999;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.7);
+}
+.custom-toolbar .p-button {
+  background-color: white;
+  color: black;
+  border: 1px solid black;
+  margin: 0 5px;
+}
+.p-button-text{
+  border-radius: 20px;
+}
+.profile-button {
+  padding: 10px 11px;
+  border-radius: 20px;
+}
+.custom-toolbar .active-button {
+  background-color: orange;
+  color: white;
+  border: orange;
+}
 
 </style>
