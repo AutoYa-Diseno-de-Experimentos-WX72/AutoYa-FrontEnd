46f728f9b140a8ee76079daa33a307e26d9a299f
Johan Moreno Vergara <U20201C105@UPC.EDU.PE>
2023-09-28 21:18:17 -0500

    Update

--- src/router/index.js
+++ src/router/index.js
@@ -4,19 +4,10 @@ import InitPropietario from "@/AutoYa/pages/inicio-propie.vue"
 import NotificacionesPropie from "@/AutoYa/pages/notificaciones-propie.vue";
 import CarRegistrationOwner from "@/AutoYa/pages/car-registration-owner.vue";
 import RentTenant from "@/AutoYa/pages/rent-tenant.vue";
-<<<<<<< HEAD
-import ProfileOwner from "@/AutoYa/pages/profile-owner.vue";
-import ProfileTenant from "@/AutoYa/pages/profile-tenant.vue";
-import RequestsTenant from "@/AutoYa/pages/requests-tenant.vue";
-import RentContractOwner from "@/AutoYa/pages/rent-contract-owner.vue";
-import RentOwner from "@/AutoYa/pages/rent-owner.vue";
-import InicioPropie from "@/AutoYa/pages/inicio-propie.vue";
-=======
 import Login from "@/AutoYa/pages/login.vue";
 import Register from "@/AutoYa/pages/register.vue";
 import Propietario from "@/AutoYa/pages/propietario.vue";
 
->>>>>>> Johan-Moreno
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -29,11 +20,6 @@ const router = createRouter({
       path: '/home',
       name: 'home',
       component: Inicio,
-    },
-    {
-      path: '/home-owner',
-      name: 'home-owner',
-      component: InicioPropie,
     },
     {
       path: "/notifications",
@@ -50,32 +36,8 @@ const router = createRouter({
       name: "rent-tenant",
       component: RentTenant,
     },
+  
     {
-<<<<<<< HEAD
-      path: '/profile-owner',
-      name: '/profile-owner',
-      component: ProfileOwner,
-    },
-    {
-      path: '/profile-tenant',
-      name: '/profile-tenant',
-      component: ProfileTenant,
-    },
-    {
-      path: '/requests-tenant',
-      name: '/requests-tenant',
-      component: RequestsTenant,
-    },
-    {
-      path: '/rent-contract-owner',
-      name: '/rent-contract-owner',
-      component: RentContractOwner,
-    },
-    {
-      path: '/rent-owner',
-      name: '/rent-owner',
-      component: RentOwner,
-=======
       path: "/login",
       name: "login",
       component: Login,
@@ -94,7 +56,6 @@ const router = createRouter({
       path: "/init-propie",
       name: "init-propie",
       component: InitPropietario,
->>>>>>> Johan-Moreno
     }
   ]
 })
