9403a18b252f717b1800c46f48107a9ebd92050d
ErickUrbizagastegui <u20201e465@upc.edu.pe>
2023-11-23 23:07:20 -0500

    cambios para incluir el api

--- src/shared/services/http-common.js
+++ src/shared/services/http-common.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:5267/api/v1',
+    baseURL: 'https://autoya.azurewebsites.net/api/v1',
     headers: { 'Content-type': 'application/json' }
 });
